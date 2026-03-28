a90f005a282bfd6f199539393b4d5af9328dfcd2
QL Software Studio <qlsoftwarestudio@gmail.com>
2026-03-28 13:23:44 -0300

    fix error ui login

--- src/services/api.ts
+++ src/services/api.ts
@@ -24,7 +24,7 @@ api.interceptors.request.use((config) => {
 api.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
+    if (error.response?.status === 401 || error.response?.status === 403) {
       localStorage.removeItem("logiair-auth");
       window.location.href = "/login";
     }
